4b987f8090d245c6412a62d76dd3f823ce7592e5
Your Name <juliangibson0607@gmail.com>
2025-10-22 12:52:52 -0400

    ideas

--- idea 1/sketch.js
+++ idea 1/sketch.js
@@ -55,6 +55,14 @@ if(dist(x,y, width, height/2) < 100){
     fill(255,0,255)
     text("secret star! :(", width/2, 100)
 }
+
+if(dist(x,y, width/1.33, height/2) < 100){
+  
+    background(100,225,0)
+    textSize(30)
+    fill(255,0,255)
+    text("go to next world!!! (click)", width/2, 100)
+}
  ellipse(width/4, height/2, 200,200)
 
  push()
@@ -87,10 +95,18 @@ ellipse(width/2, height/2, 200,200)
  image(mario,0 ,0 ,100,100)
  pop()
 
+ ellipse(width/1.33   , height/2, 200,200)
+
+ push()
+ translate(x, y)
+ rotate(r)
+ image(mario,0 ,0 ,100,100)
+ pop()
 }
 
 
-function mousePressed(){
+function mousePressed(){ 
+  
   
 }
 
